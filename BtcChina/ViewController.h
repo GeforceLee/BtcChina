05daf98fe61fa401e3896b96bc476b37bbbe14d3
GeforceLee <liyunlong880601@gmail.com>
2013-11-23 17:10:15 +0800

    init

---
+++ BtcChina/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BtcChina
+//
+//  Created by xiaochuan on 13-11-23.
+//  Copyright (c) 2013年 xiaochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BtcChinaTests/BtcChinaTests.m
@@ -0,0 +1,34 @@
+//
+//  BtcChinaTests.m
+//  BtcChinaTests
+//
+//  Created by xiaochuan on 13-11-23.
+//  Copyright (c) 2013年 xiaochuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BtcChinaTests : XCTestCase
+
+@end
+
+@implementation BtcChinaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
